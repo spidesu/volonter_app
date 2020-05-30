8fccd1a6c3d0ebbfcc195cdbf345f727a2a9942a
fedomikhail <fedomikhail@gmail.com>
2020-05-30 15:07:14 +0400

    Прописаны роуты

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('register', 'Auth\RegisterController@register');
+    Route::get('register/activate/{token}','Auth\RegisterController@registerActivate');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function() {
+        Route::post('logout', 'Auth\LoginController@logout');
+        Route::get('user', 'Auth\LoginController@user');
+        Route::resource('user','UserController');
+    });
+});
