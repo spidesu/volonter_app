a69622d4dc3ecc775b86bd215bd06f3859c03d93
fedomikhail <fedomikhail@gmail.com>
2020-05-30 19:41:12 +0400

    Добавлен функционал анкет

---
+++ app/Repositories/QuestionnaireRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Repositories;
+use App\Questionnaire as Model;
+use Illuminate\Support\Facades\Auth;
+
+class QuestionnaireRepository extends BaseRepository
+{
+
+
+    /**
+     * @return string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    public function create($data) {
+
+        if (!isset($data['user_id'])) {
+            $data['user_id'] = Auth::id();
+        }
+        $questionnaire = $this->startConditions()
+            ->create($data);
+
+        return $questionnaire;
+    }
+
+    public function show() {
+
+
+    }
+
+    public function getQuestionnaireInfo($id)
+    {
+        $columns = ['id','type_id','birth_date','experience','about','experience_about'];
+        $questionnaire = $this->startConditions()
+            ->where('id', $id)
+            ->toBase()
+            ->first($columns);
+        return $questionnaire;
+    }
+
+}
---
+++ database/migrations/2020_05_30_141255_create_questionnaire_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionnaireTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questionnaires', function (Blueprint $table) {
+            $table->id();
+            $table->integer('type_id');
+            $table->timestamp('birth_date');
+            $table->integer('experience');
+            $table->text('about');
+            $table->text('experience_about');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questionnaires');
+    }
+}
