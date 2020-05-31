b827728b3f3d3f0a7d496cbd698ffc43d1eff552
webmad <alecmei.gubin@yandex.ru>
2020-05-31 17:51:08 +0400

    Fix

--- app/Entities/Offer.php
+++ app/Entities/Offer.php
@@ -29,6 +29,6 @@ class Offer extends Model
 
     public function review()
     {
-        return $this->belongsTo(Offer::class,'offer_id');
+        return $this->hasOne(Offer::class, 'offer_id', 'id');
     }
 }
