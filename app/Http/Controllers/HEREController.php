efd98dcc060156d6482b889d6eb6d9dd1511b197
webmad <alecmei.gubin@yandex.ru>
2020-05-31 10:07:17 +0400

    Добавил urlencode

--- app/Http/Controllers/HEREController.php
+++ app/Http/Controllers/HEREController.php
@@ -8,7 +8,7 @@ class HEREController extends Controller
 {
     public function getGeocode(Request $request)
     {
-        $input = $request->search;
+        $input = urlencode($request->search);
         $search_url = "https://geocode.search.hereapi.com/v1/geocode?q={$input}&apiKey=cRbpJ4dGaX_6becnqQp_Q0oNNSoQnQu0S4nvvYmKEFU";
         $result = file_get_contents($search_url);
         return response()->json($result);
