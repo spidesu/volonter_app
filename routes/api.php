785f0a5ba5f478bc1a3ef53584f0ce6cecad53d8
webmad <alecmei.gubin@yandex.ru>
2020-05-31 15:23:31 +0400

    Debug

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::group([
     Route::get('close-vacancy','VacancyController@close');
     Route::get('vacancy/user/{id}', 'VacancyController@getUserVacancies');
     Route::resource('tag', 'TagController');
-//    Route::resource('offer', 'OfferController');
+    Route::resource('offer', 'OfferController');
     Route::resource('review','ReviewController');
 //    Route::get('offer-history/{user_id}','OfferController@offerHistory');
     Route::get('review/average_rating/{user_id}', 'ReviewController@averageRating');
