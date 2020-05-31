43dc3ff461d36f1d7b30e4c2f5dbaa8640e676bc
fedomikhail <fedomikhail@gmail.com>
2020-05-31 15:40:18 +0400

    Переработан ресурс Offer

---
+++ app/Transformers/Offers/OffersTransformerWithoutVacancy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Transformers\Offers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
+
+class OffersTransformerWithoutVacancy extends JsonResource
+{
+    public function toArray($request)
+    {
+        $data = array(
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'user' => $this->user,
+            'result' => $this->result,
+            'review' => $this->review,
+        );
+
+        return $data;
+    }
+}
--- app/Transformers/Vacancies/VacanciesTransformer.php
+++ app/Transformers/Vacancies/VacanciesTransformer.php
@@ -3,6 +3,7 @@
 namespace App\Transformers\Vacancies;
 
 use App\Transformers\Offers\OffersTransformer;
+use App\Transformers\Offers\OffersTransformerWithoutVacancy;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,7 +17,7 @@ class VacanciesTransformer extends JsonResource
             'description' => $this->description,
             'date_start'=>$this->date_start,
             'date_end'=>$this->date_end,
-            'offers' => OffersTransformer::collection($this->offers),
+            'offers' => OffersTransformerWithoutVacancy::collection($this->offers),
             'user' => $this->user,
             'city' => $this->city,
             'address' => $this->address,
